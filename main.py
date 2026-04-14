808a3c259f32ae99dfe62dbbb06d1cd320ab4a40
LorettoMolina <23308060610601@cetis61.edu.mx>
2026-04-14 08:35:19 -0500

    loretto

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+import flet as ft
+from src.controllers.UserController import AuthController
+from src.controllers.TareaController import TareaController
+from src.views.LoginView import LoginView
+from src.views.Dashboard import DashboardView
+
+def main(page: ft.Page):
+    # Instanciamos los controladores una sola vez
+    auth_ctrl = AuthController()
+    task_ctrl = TareaController()
+
+    def route_change(route):
+        page.views.clear()
+        if page.route == "/":
+            page.views.append(LoginView(page, auth_ctrl))
+        elif page.route == "/dashboard":
+            page.views.append(DashboardView(page, task_ctrl))
+       
+       # Caso de seguridad: si algo falla, mostrar texto de error
+        if not page.views:
+           page.views.append(
+               ft.View("/", [ft.Text("Error: Ruta no encontrada o vista vacía")])
+           )
+
+        page.update()
+
+    page.on_route_change = route_change
+    # Forzamos la navegación inicial
+    page.go("/")
+
+
 def main():
-    print("Hello from tareas!")
+    # Ejecución de la app
+    ft.app(target=start)
 
 
 if __name__ == "__main__":
