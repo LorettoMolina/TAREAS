4713d501f5cf56da539ff66cc86f1ee2d24b203e
LorettoMolina <23308060610601@cetis61.edu.mx>
2026-04-17 10:04:25 -0500

    actualizaciones

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 import flet as ft
-from src.controllers.UserController import AuthController
-from src.controllers.TareaController import TareaController
-from src.views.LoginView import LoginView
-from src.views.Dashboard import DashboardView
+from controllers.UserController import AuthController
+from controllers.TareasController import TareaController
+from views.LoginView import LoginView
+from views.dashbooard import DashboardView
 
-def main(page: ft.Page):
-    # Instanciamos los controladores una sola vez
+def start(page: ft.Page):
+    # instanciar controladores ua sola
     auth_ctrl = AuthController()
     task_ctrl = TareaController()
 
-    def route_change(route):
+    def route_change(e):
         page.views.clear()
+
         if page.route == "/":
+            page.add(ft.Text("Caso 1"))
             page.views.append(LoginView(page, auth_ctrl))
+
         elif page.route == "/dashboard":
             page.views.append(DashboardView(page, task_ctrl))
-       
-       # Caso de seguridad: si algo falla, mostrar texto de error
-        if not page.views:
-           page.views.append(
-               ft.View("/", [ft.Text("Error: Ruta no encontrada o vista vacía")])
-           )
 
+        if not page.views:
+            page.views.append(
+                ft.View("/", [ft.Text("Error: Ruta no encontrada o vista vacía")])
+            )
+
+        # agregas aqui las vistas que necesites
         page.update()
 
     page.on_route_change = route_change
-    # Forzamos la navegación inicial
+
     page.go("/")
 
-
 def main():
-    # Ejecución de la app
     ft.app(target=start)
-
 
 if __name__ == "__main__":
     main()
--- src/models/UserModel.py
+++ src/models/UserModel.py
@@ -2,8 +2,7 @@ import bcrypt
 from .databaseModel import Database
 
 class UsuarioModel:
-
-    def _init_(self):
+    def __init__(self):
         self.db = Database()
 
     def registrar(self, usuario_data):
@@ -13,38 +12,13 @@ class UsuarioModel:
 
         conn = self.db.get_connection()
         cursor = conn.cursor()
-
         try:
             cursor.execute(
                 "INSERT INTO usuario (nombre, email, password) VALUES (%s, %s, %s)",
-                (usuario_data.nombre, usuario_data.email, hashed_pw.decode('utf-8'))
+                (usuario_data.nombre, usuario_data.email, hashed_pw.decode())
             )
             conn.commit()
             return True
-
         except Exception as e:
             print(f"Error: {e}")
             return False
-
-        finally:
-            conn.close()
-
-    def validar_login(self, email, password):
-        conn = self.db.get_connection()
-        cursor = conn.cursor(dictionary=True)
-
-        cursor.execute(
-            "SELECT * FROM usuario WHERE email = %s",
-            (email,)
-        )
-
-        user = cursor.fetchone()
-        conn.close()
-
-        if user and bcrypt.checkpw(
-            password.encode('utf-8'),
-            user['password'].encode('utf-8')
-        ):
-            return user
-
-        return None
